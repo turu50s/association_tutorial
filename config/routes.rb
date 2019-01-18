3241726036bc74bef67132c314709ef8d78fa528
tsuruki <sigeru0216@gmail.com>
2019-01-19 08:27:29 +0900

    Add favorite

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'tweets#index'
   
+  # get 'relationships/create'
+  # get 'relationships/destroy'
   # get 'favorites/create'
   # get 'favorites/destroy'
   # get 'tweets/new'
@@ -11,9 +13,16 @@ Rails.application.routes.draw do
   # get 'useres/show'
 
   devise_for :users
+
   resources :tweets do
     resource :favorites, only: [:create, :destroy]
   end
-  resources :users
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_17_212254) do
+ActiveRecord::Schema.define(version: 2019_01_18_231311) do
 
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
