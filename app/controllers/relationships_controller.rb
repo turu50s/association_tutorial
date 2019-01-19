91d7c6288fe118f10dbacfb3a930f146a6a4b6da
tsuruki <sigeru0216@gmail.com>
2019-01-20 07:37:38 +0900

    Bug fix

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,7 +2,7 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.build(follower_id: params[:user_id])
     follow.save
-    redirect_to user_path
+    redirect_to users_path
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_many :followings, through: :active_relationships, source: :follower
 
   has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id 
-  has_many :fllowers, through: :passive_relationships, source: :following
+  has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(user)
     passive_relationships.find_by(following_id: user.id).present?
