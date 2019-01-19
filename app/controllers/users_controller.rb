d5923f7554e5b12c0ec59ba5733cb63138a3f8df
tsuruki <sigeru0216@gmail.com>
2019-01-20 07:20:34 +0900

    Add relationships

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,4 +11,14 @@ class UsersController < ApplicationController
     # @favorite_tweets = @user.favorites.map { |favorite| favorite.tweet }
     @favorite_tweets = @user.favorite_tweets
   end
+
+  def follows
+    user = User.find(params[:id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:id])
+    @users = user.followers
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ApplicationRecord
   has_many :tweets
   has_many :favorites
   has_many :favorite_tweets, through: :favorites, source: :tweet
+
+  has_many :active_relationships,  class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id 
+  has_many :fllowers, through: :passive_relationships, source: :following
+
+  def followed_by?(user)
+    passive_relationships.find_by(following_id: user.id).present?
+  end
 end
