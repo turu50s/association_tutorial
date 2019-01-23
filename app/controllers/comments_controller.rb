43764a0e15e46c665099e7000c6a5dd3c6e7b44c
tsuruki <sigeru0216@gmail.com>
2019-01-23 15:42:35 +0900

    comment処理を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,17 @@
 class CommentsController < ApplicationController
   def create
+    @tweet = Tweet.find(params[:tweet_id])
+    @comment = current_user.comments.build(comment_params)
+    @comment.tweet_id = @tweet.id
+    @comment.save
+    redirect_to tweet_path(@tweet.id)
   end
 
   def destroy
   end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,6 +13,8 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find(params[:id])
     @user = @tweet.user
+    @comments = @tweet.comments
+    @comment = current_user.comments.build
   end
 
   def create
